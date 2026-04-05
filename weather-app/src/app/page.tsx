03404d3bbd460644b61cda4fd0215eb72bd6b237
Alexan-999 <kenneth.kep@gmail.com>
2026-04-05 15:49:35 -0600

    Retrieve main layout

--- weather-app/src/app/page.tsx
+++ weather-app/src/app/page.tsx
@@ -12,7 +12,6 @@ export default function Home() {
   const handleSearch = async (city: string) => {
     console.log("Searching:", city);
 
-    // ⚠️ temporal (luego hacemos geocoding real)
     const lat = 19.43;
     const lon = -99.13;
 
@@ -21,9 +20,23 @@ export default function Home() {
   };
 
   return (
-    <main>
-      <SearchBar onSearch={handleSearch} />
-      <WeatherCard weather={weather} />
-    </main>
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-evenly py-32 px-16 bg-white dark:bg-black sm:items-start">
+  
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+                    Weather App 🌤️
+          </h1>
+          <p className="text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Find your city and avoid any more surprises over Whether!        
+          </p>
+        </div>
+
+        <SearchBar onSearch={handleSearch} />
+        <WeatherCard weather={weather} />
+
+      </main>
+    </div>
+    
   );
 }
