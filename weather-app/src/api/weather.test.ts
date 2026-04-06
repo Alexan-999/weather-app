ce19dd47b6f935424fc3050e7ca510215c5e004e
Kenneth Ek Poot <kenneth.kep@gmail.com>
2026-04-06 09:52:08 -0600

    Test for edge cases
    Increased number of test

--- weather-app/src/api/weather.test.ts
+++ weather-app/src/api/weather.test.ts
@@ -1,8 +1,9 @@
-import { getWeatherByCity } from "./weather";
+import { getWeatherByCity, CityNotFoundError, NetworkError } from "./weather";
 
 global.fetch = jest.fn();
 
 describe("getWeatherByCity", () => {
+
   beforeEach(() => {
     jest.clearAllMocks();
   });
@@ -35,4 +36,65 @@ describe("getWeatherByCity", () => {
     expect(result.city).toBe("Madrid");
     expect(result.temperature).toBe(20);
   });
+
+  // City not found
+  it("lanza error si la ciudad no existe", async () => {
+    (fetch as jest.Mock).mockResolvedValueOnce({
+      json: async () => ({ results: [] }),
+    });
+
+    await expect(getWeatherByCity("asdf")).rejects.toThrow(CityNotFoundError);
+  });
+
+  //  Network Error
+  it("lanza error de red si fetch falla", async () => {
+    (fetch as jest.Mock).mockRejectedValueOnce(new Error("fail"));
+
+    await expect(getWeatherByCity("Madrid")).rejects.toThrow(NetworkError);
+  });
+
+  // Edge case 1: format API
+  it("maneja respuesta inesperada de la API", async () => {
+    (fetch as jest.Mock)
+      .mockResolvedValueOnce({
+        json: async () => ({
+          results: [
+            {
+              name: "Madrid",
+              country: "Spain",
+              latitude: 40,
+              longitude: -3,
+            },
+          ],
+        }),
+      })
+      .mockResolvedValueOnce({
+        json: async () => ({
+          // no current_weather
+        }),
+      });
+
+    await expect(getWeatherByCity("Madrid")).rejects.toThrow();
+  });
+
+  //Edge case 2: API Fails
+  it("maneja error en weather API", async () => {
+    (fetch as jest.Mock)
+      .mockResolvedValueOnce({
+        json: async () => ({
+          results: [
+            {
+              name: "Madrid",
+              country: "Spain",
+              latitude: 40,
+              longitude: -3,
+            },
+          ],
+        }),
+      })
+      .mockRejectedValueOnce(new Error("timeout"));
+
+    await expect(getWeatherByCity("Madrid")).rejects.toThrow(NetworkError);
+  });
+
 });
